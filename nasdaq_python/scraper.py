9c04c3e62f0b97185a56c1cec416c27f8a76e33f
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-02-15 10:16:47 -0500

    Scraper

---
+++ nasdaq_python/scraper.py
@@ -0,0 +1,24 @@
+import bs4
+import requests
+class Stock:
+  def __init__(self,symbol):
+    self.symbol = symbol
+    self.r = requests.get('https://old.nasdaq.com/symbol/{}'.format(symbol.lower()))
+    self.html = self.r.text
+    self.soup = bs4.BeautifulSoup(self.html,'html.parser')
+    self.a = self.soup.find('div',{'id':'qwidget_quote'})
+    self.t = self.soup.find('div',{'id':'qwidget_pageheader'})
+  def price(self):
+    try:
+      rt = a.find_all('div')[0].text
+      if rt.strip() == self.symbol.upper():
+        return self.a.find_all('div')[1].text
+      else:
+        return self.a.find_all('div')[0].text
+    except Exception as e:
+      return None
+  def company(self):
+    try:
+      return self.t.find_all('h1')[0].text
+    except Exception as e:
+      return None

40a69d7fb2f9c275d14881672f3eacaecf23169f
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-02-15 10:10:47 -0500

    Initialization File

---
+++ nasdaq_python/__init__.py
@@ -0,0 +1,20 @@
+from scraper import Stock
+if __name__ == '__main__':
+  symbol = input('Enter Symbol:')
+  r = requests.get('https://old.nasdaq.com/symbol/{}'.format(symbol.lower()))
+  html = r.text
+  soup = bs4.BeautifulSoup(html,'html.parser')
+  a = soup.find('div',{'id':'qwidget_quote'})
+  t = soup.find('div',{'id':'qwidget_pageheader'})
+  print('Scraping {}'.format(r.url))
+  try:
+    rt = a.find_all('div')[0].text
+    if rt.strip() == symbol.upper():
+      print('Company: {}'.format(t.find_all('h1')[0].text))
+      print('Price: {}'.format(a.find_all('div')[1].text))
+    else:
+      print('Company: {}'.format(t.find_all('h1')[0].text))
+      print('Price: {}'.format(rt))
+  except Exception as e:
+    print('Symbol not found')
+    print('Error {}'.format(e))
